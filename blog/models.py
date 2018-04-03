0b28a4a52ecc2ab0b2177bf814aabd30cc316c66
NIKIL <37552869+SNFernandes24@users.noreply.github.com>
2018-04-03 04:32:40 +0100

    Finished portfolio

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,12 @@ class Blog(models.Model):
     image = models.ImageField(upload_to='images/')
     pub_date = models.DateTimeField()
     body = models.TextField()
+
+    def summary(self):
+        return self.body[:100]
+
+    def date_less_detail(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
